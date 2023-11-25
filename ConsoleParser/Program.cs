0f878d8dd64053b4810f96c49819d05c61769b3f
Анжела Санникова <sannikova.anzhela.01@outlook.com>
2023-11-25 10:33:36 +0700

    parser \

--- ConsoleParser/Program.cs
+++ ConsoleParser/Program.cs
@@ -5,12 +5,12 @@ using System.Data.SqlClient;
 
 string connectionString = $"Data Source = LAPTOP-1BQG2FKL\\SQLEXPRESS; Initial Catalog = InfoMessageDB; Integrated Security=true";
 
-string link = "https://www.banki.ru/forum/?PAGE_NAME=read&FID=61&TID=203136";
+string link = "https://habr.com/ru/articles/";
 using (IWebDriver driver = new ChromeDriver())
 {
     driver.Navigate().GoToUrl(link);
 
-    var posts = driver.FindElements(By.TagName("table"));
+    var posts = driver.FindElements(By.TagName("article"));
 
     DatabaseRepository db = new DatabaseRepository();
 
@@ -21,8 +21,8 @@ using (IWebDriver driver = new ChromeDriver())
         foreach (var post in posts)
         {
             int id = int.Parse(post.GetAttribute("id"));
-            string name = post.FindElement(By.ClassName("forum-user-name")).Text;
-            string message = post.FindElement(By.CssSelector(".hor-not-fit-element__content.display-block")).Text;
+            string name = post.FindElement(By.ClassName("tm-user-info__username")).Text;
+            string message = post.FindElement(By.CssSelector(".article-formatted-body.article-formatted-body")).Text;
 
             db.Add(id, name, message);
         }
